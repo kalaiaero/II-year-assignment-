65102d4f09c84c5036424e46f719c1abdd96077e
kalaiaero <head.intlaffairs@kcgcollege.com>
2026-07-27 12:36:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,25 @@
 import json
 import pdfplumber
 import docx
+import streamlit as st
+import google.generativeai as genai
+import json
+
+try:
+    api_key = st.secrets["GOOGLE_API_KEY"]
+
+except Exception:
+    st.error("GOOGLE_API_KEY not found in Streamlit Secrets")
+    st.stop()
+
+
+genai.configure(api_key=api_key)
+
+
+model = genai.GenerativeModel(
+    "gemini-1.5-pro"
+)
+
 def extract_text(uploaded_file):
     """Extract text from PDF or DOCX."""
 
